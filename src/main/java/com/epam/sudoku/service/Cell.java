ef54d5cc8c0d4d6345956ff477a3399833c4b887
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2019-08-21 11:11:23 +0300

    add methods

--- src/main/java/com/epam/sudoku/service/Cell.java
+++ src/main/java/com/epam/sudoku/service/Cell.java
@@ -1,4 +1,35 @@
 package com.epam.sudoku.service;
 
+import java.util.Objects;
+
 public class Cell {
+    int row;
+    int column;
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public Cell(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell cell = (Cell) o;
+        return getRow() == cell.getRow() &&
+                getColumn() == cell.getColumn();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRow(), getColumn());
+    }
 }
